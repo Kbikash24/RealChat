dc583203579f455a8cfc8a3cf24e040d084fefe3
bikash <bikashk11@gmai.com>
2024-09-22 19:28:41 +0530

    update

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,24 +1,35 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Label } from "@radix-ui/react-label";
 import { Input } from "@/components/ui/input";
 
-
-
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
-    <div className="h-[100vh] w-[100vh] flex items-center justify-center m-auto">
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center m-auto">
       <div
-        className="h-[80vh] bg-white border-2 text-opacity-90 shadow-2xl w-[80vw]
-       md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2"
+        className="h-[90vh] bg-white border-2 text-opacity-90 shadow-2xl w-[80vw]
+       md:w-[90vw] px-10 lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2"
       >
-        <div className=" flex flex-col gap-10 items-center justify-center w-[45.9vw] border-2">
+        <div className=" flex flex-col gap-10 items-center justify-center">
           <div className="flex items-center justify-center flex-col ">
             <div className="flex items-center justify-center ">
-              <h1 className="text-5xl font-bold md:text-6xl "> welcome</h1>
+              <h1 className="text-5xl font-bold md:text-5xl "> welcome</h1>
               <Image
                 src="/assets/victory.svg"
                 alt="Welcome Image"
@@ -26,63 +37,69 @@ const Auth = () => {
                 height={60}
               ></Image>
             </div>
-            <p className="font-medium text-center">Fill in the details to get started with the best chat app!</p>
-            <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
-      </TabsList>
-      <TabsContent value="account">
-        <Card>
-          <CardHeader>
-            <CardTitle>Account</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when you're done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="password">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, you'll be logged out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
+            <p className="font-medium text-center">
+              Fill in the details to get started with the best chat app!
+            </p>
+            <Tabs className="w-3/4">
+              <TabsList className="bg-transparent rounded-full w-full">
+                <TabsTrigger
+                  value="login"
+                  className=" data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  Login
+                </TabsTrigger>
+                <TabsTrigger
+                  value="signup"
+                  className=" data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  SignUp
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent className="flex flex-col gap-5 mt-10" value="login">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Input>
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Input>
+               <Button className="rounded-full">Login</Button>
+              </TabsContent>
+              <TabsContent className="flex flex-col gap-5 mt-5" value="signup">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Input>
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Input>
+                <Input
+                  placeholder="ConfirmPassword"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                ></Input>
+                <Button className="rounded-full">SignUp</Button>
+              </TabsContent>
+            </Tabs>
           </div>
-         
-
         </div>
+        <div className="hidden xl:flex justify-center items-center"><img src="/assets/login2.png" className="h-[400px]"></img></div>
       </div>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>chat App</>
+    <><h1>welcome</h1></>
   );
 }
